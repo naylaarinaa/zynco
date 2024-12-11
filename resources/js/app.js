df13262b73a25ce84cbfb6d4bdaef3dd0e91eb6b
Nayla Arina <akunayrin@gmail.com>
2024-12-11 23:50:57 +0700

    go

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,10 @@
-import './bootstrap';
-
 import Alpine from 'alpinejs';
+import axios from 'axios'; // Mengimpor axios
 
+// Konfigurasi axios
+window.axios = axios;
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// Konfigurasi Alpine.js
 window.Alpine = Alpine;
-
 Alpine.start();
